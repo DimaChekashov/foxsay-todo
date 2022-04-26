18ea1b535e03a858b152672e3f51c021a8a53bc1
DimaChekashov <foxdima99@gmail.com>
2022-04-26 18:56:21 +0300

    add TodoForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import { ITodoItem } from './types/types';
+import TodoList from './components/TodoList/TodoList';
+import TodoForm from './components/TodoForm/TodoForm';
 
 import './App.sass';
-import TodoList from './components/TodoList/TodoList';
-import Button from './components/UI/Button/Button';
-import Input from './components/UI/Input/Input';
 
 function App() {
   const [todos, setTodos] = useState<ITodoItem[]>([
@@ -12,39 +11,15 @@ function App() {
     {id: 2, title: "Javascript 2", body: "Description"},
     {id: 3, title: "Javascript 3", body: "Description"}
   ]);
-  const [todo, setTodo] = useState<{ 
-    title: string, 
-    body: string 
-  }>({
-    title: "",
-    body: ""
-  });
 
-  const addNewTodo = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-
-    setTodos([...todos, {...todo, id: Date.now()}]);
-    setTodo({ title: "", body: "" });
+  const createTodo = (newTodo: ITodoItem) => {
+    setTodos([...todos, newTodo]);
   }
 
   return (
     <div className="app">
       <div className="container">
-        <form>
-          <Input
-            value={todo.title}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTodo({ ...todo, title: e.target.value })}
-            type="text" 
-            placeholder="Todo title" 
-          />
-          <Input 
-            value={todo.body}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTodo({ ...todo, body: e.target.value })}
-            type="text" 
-            placeholder="Todo description" 
-          />
-          <Button onClick={addNewTodo}>Create todo</Button>
-        </form>
+        <TodoForm create={createTodo} />
 
         <TodoList todos={todos} title="Todo List" />
       </div>
---
+++ src/components/TodoForm/TodoForm.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { ITodoItem } from "../../types/types";
+import Button from "../UI/Button/Button";
+import Input from "../UI/Input/Input";
+
+import './TodoForm.sass';
+
+interface Props {
+  create: (newTodo: ITodoItem) => void
+}
+
+function TodoForm({ create }: Props) {
+  const [todo, setTodo] = useState<{ 
+    title: string, 
+    body: string 
+  }>({
+    title: "",
+    body: ""
+  });
+
+  const addNewTodo = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    
+    const newTodo = {
+      ...todo, id: Date.now()
+    }
+
+    create(newTodo);
+
+    setTodo({ title: "", body: "" });
+  }
+
+  return (
+    <form className="todo-form">
+      <Input
+        value={todo.title}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTodo({ ...todo, title: e.target.value })}
+        type="text" 
+        placeholder="Todo title" 
+      />
+      <Input 
+        value={todo.body}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTodo({ ...todo, body: e.target.value })}
+        type="text" 
+        placeholder="Todo description" 
+      />
+      <Button onClick={addNewTodo}>Create todo</Button>
+    </form>
+  )
+}
+
+export default TodoForm;
