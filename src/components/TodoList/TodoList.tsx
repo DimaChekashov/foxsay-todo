721d7a6e7fc892f8a4944807a3ba12af30ecea87
DimaChekashov <foxdima99@gmail.com>
2022-04-28 00:02:24 +0300

    add Animation

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ITodoItem } from '../../types/types';
 import TodoItem from '../TodoItem/TodoItem';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import './TodoList.sass';
 
@@ -15,10 +16,20 @@ const TodoList: React.FC<Props> = ({ todos, title, remove }) => {
     <>
         <h1 className="title">{title}</h1>
 
-        {todos.length !== 0
-          ? todos.map((todo, index) => <TodoItem remove={remove} number={++index} key={todo.id} todo={todo} />)
-          : <div className="empty-title">Todo is empty!</div>
-        }
+          {todos.length !== 0
+            ? <TransitionGroup>
+                {todos.map((todo, index) => (
+                  <CSSTransition
+                    key={todo.id}
+                    timeout={500}
+                    classNames="todo"
+                  >
+                    <TodoItem remove={remove} number={++index} todo={todo} />
+                  </CSSTransition>
+                ))}
+              </TransitionGroup>
+            : <div className="empty-title">Todo is empty!</div>
+          }
     </>
   )
 };
