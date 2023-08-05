854042d3f1984965a429c1554b491d3659315961
DimaChekashov <foxdima99@gmail.com>
2023-08-05 15:24:56 +0200

    split footer

--- native/script.js
+++ native/script.js
@@ -3,7 +3,7 @@ class DomManipulator {
         return document.querySelector(className);
     }
 
-    createElement(tagName, className, enterTarget, innerText, type) {
+    createElement(tagName, className, enterTarget, innerText, attributes) {
         const element = document.createElement(tagName);
 
         if(Array.isArray(className)) {
@@ -12,8 +12,8 @@ class DomManipulator {
             element.classList.add(className);
         }
 
-        if(type) {
-            this.setAttr(element, type.attrName, type.value);
+        if(attributes) {
+            this.setAttr(element, attributes);
         }
 
         if (innerText) {
@@ -45,12 +45,38 @@ class DomManipulator {
         domElement.classList.add(className);
     }
 
-    setAttr(dom, attrName, value) {
-        dom.setAttribute(attrName, value);
+    setAttr(dom, attributes) {
+        if(!attributes) return;
+
+        if(Array.isArray(attributes)) {
+            for(let i = 0; i < attributes.length; i++) {
+                const splited = attributes[i].split(":");
+                dom.setAttribute(splited[0], splited[1]);
+            }
+        } else {
+            const splited = attributes.split(":");
+            dom.setAttribute(splited[0], splited[1]);
+        }
     }
 }
 
 class TodoItem {
+}
+
+class Footer {
+    constructor(parentBlock) {
+        this.container = dom.createElement("div", "todo-footer", parentBlock);
+        this._todoNameInput = dom.createElement("input", "todo-input", this.container, null, ["type:text", "placeholder:What you want to do?"]);
+        this._todoAddItemBtn = dom.createElement("button", "todo-add", this.container, "Add Todo", "type:button");
+    }
+
+    get inputDom() {
+        return this._todoNameInput;
+    }
+
+    get addBtnDom() {
+        return this._todoAddItemBtn;
+    }
 }
 
 class TodoApp {
@@ -59,14 +85,16 @@ class TodoApp {
         this._todos = todos;
         this.mainBlock = dom.createElement("div", "todo", root);
         this.header = dom.createElement("div", "todo-header", this.mainBlock);
+        this.searchBlock = dom.createElement("div", "todo-search", this.mainBlock);
         this.body = dom.createElement("div", "todo-body", this.mainBlock);
-        this.footer = dom.createElement("div", "todo-footer", this.mainBlock);
+        this.footer = new Footer(this.mainBlock);
     }
 
     init() {
         dom.setText(this.header, this._name);
-        this.fillFooter();
+        this.fillSearchBlock();
         this.loadTodos();
+        this.addTodo();
     }
 
     loadTodos() {
@@ -78,8 +106,8 @@ class TodoApp {
     createTodo(todo, index) {
         const todoItem = dom.createElement("div", "todo-item", this.body);
         const todoText = dom.createElement("div", "todo-item__text", todoItem);
-        const todoCompleteBtn = dom.createElement("button", ["todo-item__btn", "todo-item__btn_complete"], todoItem, "&#10003;", {attrName: "type", value: "button"});
-        const todoDeleteBtn = dom.createElement("button", ["todo-item__btn", "todo-item__btn_delete"], todoItem, "&#10006;", {attrName: "type", value: "button"});
+        const todoCompleteBtn = dom.createElement("button", ["todo-item__btn", "todo-item__btn_complete"], todoItem, "&#10003;", "type:button");
+        const todoDeleteBtn = dom.createElement("button", ["todo-item__btn", "todo-item__btn_delete"], todoItem, "&#10006;", "type:button");
         
         dom.setText(todoText, todo.title);
         
@@ -100,17 +128,18 @@ class TodoApp {
         dom.deleteChildNodes(this.body);
     }
 
-    fillFooter() {
-        const todoInput = dom.createElement("input", "todo-input", this.footer, null, {attrName: "type", value: "text"});
-        const todoAddItem = dom.createElement("button", "todo-add", this.footer, "Add Todo", {attrName: "type", value: "button"});
+    fillSearchBlock() {
+        const searchInput = dom.createElement("input", "todo-search__input", this.searchBlock, null, ["type:text", "placeholder:Search..."]);
+    }
 
-        todoAddItem.addEventListener("click", () => {
-            if(!todoInput.length) {
+    addTodo() {
+        this.footer.addBtnDom.addEventListener("click", () => {
+            if(!this.footer.inputDom.length) {
                 this._todos.push({
-                    title: todoInput.value,
+                    title: this.footer.inputDom.value,
                     status: false,
                 });
-                todoInput.value = "";
+                this.footer.inputDom.value = "";
                 this.render();
             }
         })
