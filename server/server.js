79ff7db2b28fed4bd6d552ac8319d272618c1ef8
DimaChekashov <foxdima99@gmail.com>
2025-03-19 08:20:36 +0300

    add db

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 import 'dotenv/config';
 import app from "./app.js";
+import connectDB from './config/db.js';
 
 const port = process.env.PORT || 3000;
+
+connectDB();
 
 app.listen(port, () => {
 	console.log(`Server running on port http://localhost:${port}`);
