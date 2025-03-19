4619d6506c871f263af7bbd0f2ff5b1f265cdb38
DimaChekashov <foxdima99@gmail.com>
2025-03-19 10:33:27 +0300

    add tailwind and antd

---
+++ react-todo/src/app/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div>
+      <h1 className="font-bold text-3xl">App</h1>
+      <Button>Hello world!</Button>
+    </div>
+  );
+}
+
+export default App;
