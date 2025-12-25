70bac76f6a9a9ead26100b543d58aac2bd513d7e
DimaChekashov <foxdima99@gmail.com>
2025-12-26 01:21:04 +0300

    Create my first rust http server

--- rust-server/src/main.rs
+++ rust-server/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world 2!");
+use warp::{Filter};
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::path::end().map(|| "Welcome to the server!");
+
+    let routes = hello.with(warp::log("server"));
+
+    println!("Server started on http://localhost:8000");
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 8000))
+        .await;
 }
