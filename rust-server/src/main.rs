011b0b58f3df5dc64f448d45b8faa48ccfaab292
DimaChekashov <foxdima99@gmail.com>
2025-12-26 02:05:43 +0300

    Add todos endpoints

--- rust-server/src/main.rs
+++ rust-server/src/main.rs
@@ -2,12 +2,31 @@ use warp::{Filter};
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::path::end().map(|| "Welcome to the server!");
+    let get_todos = warp::path!("todos")
+        .and(warp::get())
+        .map(|| {
+            warp::reply::json(&serde_json::json!([{
+                "id": 1,
+                "title": "Task 1",
+                "isReady": false
+            },
+            {
+                "id": 2,
+                "title": "Task 2",
+                "isReady": true
+            },
+            {
+                "id": 3,
+                "title": "Task 3",
+                "isReady": false
+            }]))
+        });
 
-    let routes = hello.with(warp::log("server"));
+    let routes = get_todos
+        .with(warp::log("api"));
 
-    println!("Server started on http://localhost:8000");
+    println!("Server started on http://localhost:8080");
     warp::serve(routes)
-        .run(([127, 0, 0, 1], 8000))
+        .run(([127, 0, 0, 1], 8080))
         .await;
 }
