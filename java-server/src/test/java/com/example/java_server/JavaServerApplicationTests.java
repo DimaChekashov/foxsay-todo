2ec5122029750fe0f15b8749f9266aaf27696702
DimaChekashov <foxdima99@gmail.com>
2025-12-30 22:43:20 +0300

    Create my first spring app

--- java-server/src/test/java/com/example/java_server/JavaServerApplicationTests.java
+++ java-server/src/test/java/com/example/java_server/JavaServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.java_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class JavaServerApplicationTests {
 
 	@Test
 	void contextLoads() {
